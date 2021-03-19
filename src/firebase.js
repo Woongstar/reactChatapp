6b3af79164d6b71db82b8f6ffa4b42ad4925d35f
Jiwoong Yang <qualtypo@gmail.com>
2021-03-19 23:45:24 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+  import firebase from 'firebase/app'
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyCMw8kNt_uHYIQ6ubE6tJr2tMWgR3CDNWE",
+    authDomain: "react-firebase-chatapp-3c115.firebaseapp.com",
+    projectId: "react-firebase-chatapp-3c115",
+    storageBucket: "react-firebase-chatapp-3c115.appspot.com",
+    messagingSenderId: "877809346079",
+    appId: "1:877809346079:web:f2785c81f3780b9f88172a",
+    measurementId: "G-158THH84EG"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
