ee63e7c813f6c869befa571a6740305143d78165
Jiwoong Yang <qualtypo@gmail.com>
2021-03-27 01:01:04 +0900

    add register page

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,18 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
   return (
-    <div>
+    <div className="auth-wrapper">
+      <div style={{ textAlign: "center" }}>
+        <h3>Register</h3>
+      </div>
       <form>
-        <label>Example</label>
-        <input name="example" defaultValue="test" />
-        <label>ExampleRequired</label>
+        <label>E-mail</label>
         <input
-          name="exampleRequired"
+          name="email"
+          type="email"
+          // ref={register({ required: true, maxLength: 10 })}
+        />
+        {/* {errors.exampleRequired && <p>This field is required</p>} */}
+        <label>Name</label>
+        <input
+          name="name"
+          // ref={register({ required: true, maxLength: 10 })}
+        />
+        {/* {errors.exampleRequired && <p>This field is required</p>} */}
+        <label>Password</label>
+        <input
+          name="password"
+          type="password"
+          // ref={register({ required: true, maxLength: 10 })}
+        />
+        {/* {errors.exampleRequired && <p>This field is required</p>} */}
+        <label>Password confirm</label>
+        <input
+          name="password"
+          type="password"
           // ref={register({ required: true, maxLength: 10 })}
         />
         {/* {errors.exampleRequired && <p>This field is required</p>} */}
         <input type="submit" />
+        <Link style={{ color: "gray", textDecoration: "none" }} to="login">
+          이미 아이디가 있다면..
+        </Link>
       </form>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
-import 'firebase/storage';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+import "firebase/storage";
 
-  var firebaseConfig = {
-    apiKey: "AIzaSyCMw8kNt_uHYIQ6ubE6tJr2tMWgR3CDNWE",
-    authDomain: "react-firebase-chatapp-3c115.firebaseapp.com",
-    projectId: "react-firebase-chatapp-3c115",
-    storageBucket: "react-firebase-chatapp-3c115.appspot.com",
-    messagingSenderId: "877809346079",
-    appId: "1:877809346079:web:f2785c81f3780b9f88172a",
-    measurementId: "G-158THH84EG"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyCMw8kNt_uHYIQ6ubE6tJr2tMWgR3CDNWE",
+  authDomain: "react-firebase-chatapp-3c115.firebaseapp.com",
+  projectId: "react-firebase-chatapp-3c115",
+  storageBucket: "react-firebase-chatapp-3c115.appspot.com",
+  messagingSenderId: "877809346079",
+  appId: "1:877809346079:web:f2785c81f3780b9f88172a",
+  measurementId: "G-158THH84EG",
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
